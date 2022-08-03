344ce67b091d792849f2a3637783ab742cd2d55f
Abhishek Yadav <86714900+fAwkes1101@users.noreply.github.com>
2022-08-04 00:31:40 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,21 @@
+//connect to database
+let db;
+let openRequest = indexedDB.open('myDatabase');
+let a = 123;
+openRequest.addEventListener('success', () => {
+    console.log('db connected');
+    db = openRequest.result;
+})
+
+openRequest.addEventListener("upgradeneeded", () => {
+    console.log("db upgraded OR initalised db ");
+    db = openRequest.result;
+
+    db.createObjectStore('video', { keyPath: "id" });
+    db.createObjectStore("image", { keyPath: "id" });
+});
+
+openRequest.addEventListener("error", () => {
+    console.log("db error");
+});
+
